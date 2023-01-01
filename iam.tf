3f10ce19c480099572cb3452af8429deb119f47c
Eugenio <eugenio.araujo@cwi.com.br>
2023-01-01 19:28:21 -0300

    commit

--- iam.tf
+++ iam.tf
@@ -5,21 +5,20 @@ locals {
 }
 
 resource "aws_iam_role" "iam_role" {
-  name               = "${local.namespace}-tf-assume-role"
-  assume_role_policy = <<-EOF
+  name = "${local.namespace}-tf-assume-role"
+  assume_role_policy = jsonencode(
+    {
+      Version : "2012-10-17",
+      Statement : [
         {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Action": "sts:AssumeRole",
-                    "Principal": {
-                        "AWS": ${jsonencode(local.principal_arns)}
-                    },
-                    "Effect": "Allow"
-                }
-            ]
+          Action : "sts:AssumeRole",
+          Principal : {
+            AWS : jsonencode(local.principal_arns)
+          },
+          Effect : "Allow"
         }
-    EOF
+      ]
+  })
   tags = {
     ResourceGroup = local.namespace
   }
