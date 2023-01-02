f1a41018a4587a83d57b68046f6eb1bb9663b7d1
Eugenio <eugenio.araujo@cwi.com.br>
2023-01-01 23:08:35 -0300

    dfdfdfd

--- iam.tf
+++ iam.tf
@@ -27,7 +27,7 @@ resource "aws_iam_role" "iam_role" {
 data "aws_iam_policy_document" "policy_doc" {
   statement {
     actions   = ["s3:ListBucket", ]
-    resources = ["arn:aws:s3:::${aws_s3_bucket_versioning.s3_bucket.name}"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket_versioning.s3_bucket.}"]
   }
   statement {
     actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
