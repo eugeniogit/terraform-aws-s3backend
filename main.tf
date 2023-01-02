3ef7abef3f313aeafc698b3a7763181430854cba
Eugenio <eugenio.araujo@cwi.com.br>
2023-01-01 22:44:31 -0300

    ddd

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_s3_bucket_versioning" "s3_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "s3_bucket" {
-  bucket                  = aws_s3_bucket.s3_bucket.id
+  bucket                  = aws_s3_bucket_versioning.s3_bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
