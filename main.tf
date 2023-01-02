5e049204def83a87a1eb96cd13758be4176eeff5
Eugenio <eugenio.araujo@cwi.com.br>
2023-01-01 22:39:55 -0300

    mains

--- main.tf
+++ main.tf
@@ -32,12 +32,9 @@ resource "aws_kms_key" "kms_key" {
   }
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
+resource "aws_s3_bucket_versioning" "s3_bucket" {
   bucket        = "${local.namespace}-state-bucket"
   force_destroy = var.force_destroy_state
-  versioning {
-    enabled = true
-  }
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
